c48fe9a4f161ec0c04da4dda45c0994e0076bb78
gaurovgiri <gaurovgiri@gmail.com>
2023-06-29 17:35:57 +0545

    Add OnBoard Screen

---
+++ android/app/src/main/kotlin/np/com/gauravgiri/mero_dokan/MainActivity.kt
@@ -0,0 +1,6 @@
+package np.com.gauravgiri.mero_dokan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/onboard/presentation/onboard_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboard/flutter_onboard.dart';
+
+class OnBoardScreen extends StatelessWidget {
+  final PageController _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OnBoard(
+        pageController: _pageController,
+        // Either Provide onSkip Callback or skipButton Widget to handle skip state
+        onSkip: () {
+          // print('skipped');
+        },
+        // Either Provide onDone Callback or nextButton Widget to handle done state
+        onDone: () {
+          // print('done tapped');
+        },
+        onBoardData: onBoardData,
+        titleStyles: const TextStyle(
+          color: Colors.deepOrange,
+          fontSize: 18,
+          fontWeight: FontWeight.w900,
+          letterSpacing: 0.15,
+        ),
+        descriptionStyles: TextStyle(
+          fontSize: 16,
+          color: Colors.brown.shade300,
+        ),
+        pageIndicatorStyle: const PageIndicatorStyle(
+          width: 100,
+          inactiveColor: Colors.deepOrangeAccent,
+          activeColor: Colors.deepOrange,
+          inactiveSize: Size(8, 8),
+          activeSize: Size(12, 12),
+        ),
+        // Either Provide onSkip Callback or skipButton Widget to handle skip state
+        skipButton: TextButton(
+          onPressed: () {
+            // print('skipButton pressed');
+          },
+          child: const Text(
+            "Skip",
+            style: TextStyle(color: Colors.deepOrangeAccent),
+          ),
+        ),
+        // Either Provide onDone Callback or nextButton Widget to handle done state
+        nextButton: OnBoardConsumer(
+          builder: (context, ref, child) {
+            final state = ref.watch(onBoardStateProvider);
+            return InkWell(
+              onTap: () => _onNextTap(state),
+              child: Container(
+                width: 230,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: const LinearGradient(
+                    colors: [Colors.redAccent, Colors.deepOrangeAccent],
+                  ),
+                ),
+                child: Text(
+                  state.isLastPage ? "Done" : "Next",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _onNextTap(OnBoardState onBoardState) {
+    if (!onBoardState.isLastPage) {
+      _pageController.animateToPage(
+        onBoardState.page + 1,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOutSine,
+      );
+    } else {
+      //print("nextButton pressed");
+    }
+  }
+}
+
+final List<OnBoardModel> onBoardData = [
+  const OnBoardModel(
+    title: "Buy different items online",
+    description: "Let your shopping beast unleash",
+    imgUrl: "assets/images/onboard/onboard_one.png",
+  ),
+  const OnBoardModel(
+    title: "Add your favorite items in your cart.",
+    description: "There are thousands of items that you can pick from.",
+    imgUrl: 'assets/images/onboard/onboard_two.png',
+  ),
+  const OnBoardModel(
+    title: "Buy your dream.",
+    description: "Buy and get your item within a day.",
+    imgUrl: 'assets/images/onboard/onboard_three.png',
+  ),
+];
